bcfc19621f7e748ae3162329aaa200b70f7bb2cf
Simone Anile <23080613+simusr2@users.noreply.github.com>
2022-08-02 10:44:04 +0000

    Updated typescript configuration with custom types

---
+++ scripts/Home.ts
@@ -0,0 +1,11 @@
+function CreateTable() {
+  let a: Tabulator.OptionsCell;
+  
+    //const table: Tabulator = new Tabulator();
+    $("#test").text("Ciao");
+}
+function ConsoleLog(message: string): void {
+  console.log(message);
+}
+
+ConsoleLog("Hello, World!");
---
+++ wwwroot/js/Home.js
@@ -0,0 +1,9 @@
+function CreateTable() {
+    let a;
+    //const table: Tabulator = new Tabulator();
+    $("#test").text("Ciao");
+}
+function ConsoleLog(message) {
+    console.log(message);
+}
+ConsoleLog("Hello, World!");
